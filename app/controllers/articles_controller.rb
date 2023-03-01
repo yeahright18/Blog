3f1109e06271338308bdb5404a1ddbc8d4b90357
yeahright18 <szymonsk312@gmail.com>
2023-03-01 11:32:47 +0100

    Add one to many association user/articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,10 +13,12 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    
   end
 
   def create
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
       flash[:notice] = "Article was created succesfully."
       redirect_to @article
