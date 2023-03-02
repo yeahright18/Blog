aca2ba3c6eb14c31da0a8c9c22a6df353c0d37a7
yeahright18 <szymonsk312@gmail.com>
2023-03-02 12:08:32 +0100

    Add users create action

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_01_101554) do
+ActiveRecord::Schema.define(version: 2023_03_01_160257) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2023_03_01_101554) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
