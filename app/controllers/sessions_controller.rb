d74260a81bb102940064a777d394c87bad588d19
yeahright18 <szymonsk312@gmail.com>
2023-03-06 17:28:05 +0100

    Add login and logout functionality

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+def new
+end
+
+def create
+  user = User.find_by(email: params[:session][:email].downcase)
+  if user && user.authenticate(params[:session][:password])
+    session[:user_id] = user.id
+    flash[:notice] = "Zalogowano pomyslnie"
+    redirect_to user
+  else
+    flash.now[:alert] = "Nieprawidlowe haslo badz email"
+    render 'new'
+  end
+end
+
+def destroy
+  session[:user_id] = nil
+  flash[:notice] = "Wylogowano pomyslnie"
+  redirect_to root_path
+end
+
+end
