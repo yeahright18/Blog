8bab59013217303c506a446a3d8def3fddcc05cb
yeahright18 <szymonsk312@gmail.com>
2023-03-06 18:13:15 +0100

    Authentication helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
 
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
 
   end
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Witam w legendarnym i tajnym JDM BLOG #{@user.username} "
       redirect_to articles_path
     else
@@ -25,12 +26,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
 
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Twoje konto zostalo zaktualizowane"
       redirect_to @user
@@ -42,6 +41,12 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
