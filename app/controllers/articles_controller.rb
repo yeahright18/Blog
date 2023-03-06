b2bd514585860108e29cd04ac72fc932101a8087
yeahright18 <szymonsk312@gmail.com>
2023-03-06 16:48:47 +0100

    Adding pagination to app

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,10 +2,11 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def show
+
   end
 
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -13,7 +14,7 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    
+
   end
 
   def create
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,12 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @articles = @user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+
+  end
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -28,7 +33,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Twoje konto zostalo zaktualizowane"
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'edit'
     end
