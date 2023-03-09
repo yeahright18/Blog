6f636bb3e504558188211ce07826d77f8ae6dfc6
yeahright18 <szymonsk312@gmail.com>
2023-03-09 15:00:34 +0100

    Delete user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :require_user, only: [:edit, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
@@ -40,14 +40,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Konto i wszystkie powiazane z nim wpisy zostaly usuniete"
+    redirect_to root_path
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
 
   private
-
-
 
   def set_user
     @user = User.find(params[:id])
@@ -59,5 +64,5 @@ class UsersController < ApplicationController
       redirect_to @user
     end
   end
-  
+
 end
